cbcfa22148151e822477f4589678f6b75d7adaa1
pavan6476252 <meesalapavan2003@gmail.com>
2023-09-13 19:57:23 +0530

    added images

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import { CopyBlock , dracula } from 'react-code-blocks';
 
 
 function App() {
+    let json = {
+        1: './assets/1.png',
+        2: './assets/2.png',
+        3: './assets/3.png',
+        4: './assets/4.png',
+        5: './assets/5.png',
+        6: './assets/6.png',
+        7: './assets/7.png',
+    }
+
+    console.log(json)
   return (
     <div className="App">
       <h1>red black</h1>
